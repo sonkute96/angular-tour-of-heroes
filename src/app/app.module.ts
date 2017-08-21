153b9278333d6713f2c1cbbe493eb89963a37137
Son Pham Minh <intern.pmson@tma.com.vn>
2017-08-21 16:58:17 +0700

    upload project

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './service/hero.service';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from "./dashboard.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+
+@NgModule({
+  declarations: [
+   AppComponent,
+   HeroesComponent,
+   HeroDetailComponent,
+   DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
